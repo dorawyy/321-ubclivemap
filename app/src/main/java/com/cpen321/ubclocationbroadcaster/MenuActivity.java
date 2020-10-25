2f7a909de5209133b0c9774a0191715f3f1c5b03
Sebastian <gsebass1stu>
2020-10-24 21:16:46 -0700

    menu mostly done

--- app/src/main/java/com/cpen321/ubclocationbroadcaster/MenuActivity.java
+++ app/src/main/java/com/cpen321/ubclocationbroadcaster/MenuActivity.java
@@ -2,13 +2,58 @@ package com.cpen321.ubclocationbroadcaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+    private Button viewMapButton;
+    private Button listActivitesButton;
+    private Button myActivityButton;
+    private Button signOutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        viewMapButton = findViewById(R.id.view_map_btn);
+        viewMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mapIntent = new Intent(MenuActivity.this, MainMapsActivity.class);
+                startActivity(mapIntent);
+            }
+        });
+        /*
+        listActivitesButton = findViewById(R.id.list_activites_btn);
+        listActivitesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent activitesIntent = new Intent(MenuActivity.this, MainMapsActivity.class);
+                startActivity(activitesIntent);
+            }
+        });
+
+        viewMapButton = findViewById(R.id.view_map_btn);
+        viewMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mapIntent = new Intent(MenuActivity.this, MainMapsActivity.class);
+                startActivity(mapIntent);
+            }
+        });
+        */
+        signOutButton = findViewById(R.id.sign_out_btn);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = new Intent(MenuActivity.this, MainActivity.class);
+                startActivity(signInIntent);
+            }
+        });
     }
+
 }

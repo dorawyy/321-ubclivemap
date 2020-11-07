81c2a8a4b1d4518455e4c05d94d04e67e4ba43a3
Sebastian <gsebass1stu>
2020-11-07 13:18:11 -0800

    Removed unnecessary imports from tests.

--- app/src/androidTest/java/com/cpen321/ubclocationbroadcaster/MainActivityTest.java
+++ app/src/androidTest/java/com/cpen321/ubclocationbroadcaster/MainActivityTest.java
@@ -1,24 +1,18 @@
 package com.cpen321.ubclocationbroadcaster;
 
 import androidx.test.espresso.Espresso;
-import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
-import androidx.test.rule.ActivityTestRule;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.*;
 
 public class MainActivityTest {
     @Rule
--- app/src/androidTest/java/com/cpen321/ubclocationbroadcaster/ProfileActivityTest.java
+++ app/src/androidTest/java/com/cpen321/ubclocationbroadcaster/ProfileActivityTest.java
@@ -1,16 +1,8 @@
 package com.cpen321.ubclocationbroadcaster;
 
-import android.content.Context;
-import android.provider.ContactsContract;
-import android.widget.EditText;
-import android.widget.ListView;
-
 import androidx.test.espresso.Espresso;
-import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.rule.ActivityTestRule;
 
 import org.junit.After;
 import org.junit.Before;
@@ -18,22 +10,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
-import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).

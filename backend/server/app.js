14080f5638664710b41f2c8eff9862643dac4d8b
solomanz <zyad_ben-suleiman@alumni.ubc.ca>
2020-11-07 12:07:17 -0800

    popup notifications

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var bcrypt = require("bcryptjs");
+var admin = require("firebase-admin");
+var serviceAccount = require("./accountkey.json");
 
 var Account = require("./__models__/models").Account;
 var Profile = require("./__models__/models").Profile;
@@ -17,6 +19,42 @@ function formatResponse(successVal, status, val) {
     }
     return resp;
 }
+
+/****************************************************************************
+ ********************** PUSH NOTIFICATIONS BASE *****************************
+****************************************************************************/
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://cpen321projectworking.firebaseio.com"
+})
+
+module.exports.admin = admin
+
+const notification_options = {
+    priority: "high",
+    timeToLive: 60 * 60 * 24
+};
+
+var REG_TOKEN;
+app.post('/givetoken', (req,res) => {
+    REG_TOKEN = req.body.token; 
+});
+
+app.post('/firebase/notification', (req, res)=>{
+    const registrationToken = req.body.registrationToken
+    const message = req.body.message
+    const options =  notification_options
+
+    
+    admin.messaging().sendToDevice(registrationToken, req.body.payload, options)
+    .then( response => {
+        res.status(200).send("Notification sent successfully")
+    })
+    .catch( error => {
+        console.log(error);
+    });
+})
 
 /****************************************************************************
  ********************** ACCOUNT DATA BASE API CALLS *************************

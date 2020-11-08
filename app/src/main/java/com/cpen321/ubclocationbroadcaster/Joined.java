91b8268ff042403866974969ee68de80e482be0e
Jaskirat <jaskiratubc@gmail.com>
2020-11-08 03:49:15 -0800

    Changed joined.java to suite codacy

--- app/src/main/java/com/cpen321/ubclocationbroadcaster/Joined.java
+++ app/src/main/java/com/cpen321/ubclocationbroadcaster/Joined.java
@@ -33,6 +33,102 @@ public class Joined extends AppCompatActivity {
 
     private boolean joinStatus;
 
+    private JSONObject getJoinObject(String aid, String username){
+        JSONObject joinObject = new JSONObject();
+        try{
+            joinObject.put("aid", aid);
+            joinObject.put("user", username);
+            Log.d("Join", "Created joinObject" + joinObject.getString("aid") + joinObject.getString("user"));
+        }catch (JSONException e){
+            Log.d("Join", "Error: Could not create joinObject");
+            e.printStackTrace();
+        }
+        return joinObject;
+    }
+
+    private void updateActivityDB (JSONObject joinObject){
+        //Update the Activity Database Entry's usernames.
+        //That is: Add the username of the current user to the users of the activity
+
+        final RequestQueue ao = Volley.newRequestQueue(this);
+        JsonObjectRequest activity_object = new JsonObjectRequest(Request.Method.POST, UserdetailsUtil.getURL() + "/activities/join", joinObject,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            Log.d("Join", "Entered activity_object");
+                            joinStatus = (boolean) response.get("success");
+                            Log.d("Join","joinStatus: " + joinStatus);
+                            Log.d("Join","response success: " + response.get("success").toString());
+                            if(joinStatus){
+                                Toast.makeText(Joined.this, "Successfully Joined Activity", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            Log.d("Join", "Error: Could not extract joinStatus");
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Joined.this, "Connection error while joining, try again later!", Toast.LENGTH_SHORT).show();
+                error.printStackTrace();
+            }
+        });
+
+        ao.add(activity_object);
+    }
+
+    private JSONObject getUserObject (String username, String aid){
+        JSONObject userObject = new JSONObject();
+        try{
+            userObject.put("username", UserdetailsUtil.username);
+            userObject.put("aid", SortedlistclassUtil.activity_to_be_displayed);
+            Log.d("Join", "Created joinObject" + userObject.getString("aid") + userObject.getString("username"));
+        }catch (JSONException e){
+            e.printStackTrace();
+            Log.d("Join", "Error: Could not create userObject");
+        }
+        return userObject;
+    }
+
+    private void updateUserDB (JSONObject userObject){
+        final RequestQueue ao = Volley.newRequestQueue(this);
+        JsonObjectRequest user_join = new JsonObjectRequest(Request.Method.POST, UserdetailsUtil.getURL() + "/profiles/join", userObject,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            boolean userJoinStatus = false;
+                            Log.d("userJoin", "Entered user_join");
+                            userJoinStatus = (boolean) response.get("success");
+                            Log.d("userJoin", "userJoinStatus: " + userJoinStatus);
+                            if(userJoinStatus){
+                                //Update Locally
+                                UserdetailsUtil.inactivity = true;
+                                UserdetailsUtil.activityID = SortedlistclassUtil.activity_to_be_displayed;
+
+                                //For Debugging
+                                Log.d("UserDBUpdate", "Successfully Updated User to reflect Joined Activity");
+                                Log.d("UserDBUpdate", " : " + UserdetailsUtil.inactivity);
+                                Log.d("UserDBUpdate", UserdetailsUtil.activityID);
+
+                            }
+                        } catch (JSONException e) {
+                            Log.d("UserDBUpdate", "UNSuccessfully Updated User to reflect Joined Activity");
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Joined.this, "Connection error while joining, try again later!", Toast.LENGTH_SHORT).show();
+                error.printStackTrace();
+            }
+        });
+        ao.add(user_join);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,90 +144,14 @@ public class Joined extends AppCompatActivity {
             Log.d("Join", "It seems inactivity was false.");
 
             //Object Required by Backend to update the Activity users
-            JSONObject joinObject = new JSONObject();
-            try{
-                joinObject.put("aid", SortedlistclassUtil.activity_to_be_displayed);
-                joinObject.put("user", UserdetailsUtil.username);
-                Log.d("Join", "Created joinObject" + joinObject.getString("aid") + joinObject.getString("user"));
-            }catch (JSONException e){
-                Log.d("Join", "Error: Could not create joinObject");
-                e.printStackTrace();
-            }
+            JSONObject joinObject = getJoinObject(SortedlistclassUtil.activity_to_be_displayed,UserdetailsUtil.username);
 
-
-            //Update the Activity Database Entry's usernames.
-            //That is: Add the username of the current user to the users of the activity
-            JsonObjectRequest activity_object = new JsonObjectRequest(Request.Method.POST, UserdetailsUtil.getURL() + "/activities/join", joinObject,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            try {
-                                Log.d("Join", "Entered activity_object");
-                                joinStatus = (boolean) response.get("success");
-                                Log.d("Join","joinStatus: " + joinStatus);
-                                Log.d("Join","response success: " + response.get("success").toString());
-                                if(joinStatus){
-                                    Toast.makeText(Joined.this, "Successfully Joined Activity", Toast.LENGTH_SHORT).show();
-                                }
-                            } catch (JSONException e) {
-                                Log.d("Join", "Error: Could not extract joinStatus");
-                                e.printStackTrace();
-                            }
-
-                        }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(Joined.this, "Connection error while joining, try again later!", Toast.LENGTH_SHORT).show();
-                    error.printStackTrace();
-                }
-            });
-
-            ao.add(activity_object);
+            updateActivityDB(joinObject);
 
             //Update the UserDetails locally and the UserDB backend Database to reflect that the user is in a activity now.
-            JSONObject userObject = new JSONObject();
-            try{
-                userObject.put("username", UserdetailsUtil.username);
-                userObject.put("aid", SortedlistclassUtil.activity_to_be_displayed);
-                Log.d("Join", "Created joinObject" + userObject.getString("aid") + userObject.getString("username"));
-            }catch (JSONException e){
-                e.printStackTrace();
-                Log.d("Join", "Error: Could not create userObject");
-            }
-            JsonObjectRequest user_join = new JsonObjectRequest(Request.Method.POST, UserdetailsUtil.getURL() + "/profiles/join", userObject,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            try {
-                                boolean userJoinStatus = false;
-                                Log.d("userJoin", "Entered user_join");
-                                userJoinStatus = (boolean) response.get("success");
-                                Log.d("userJoin", "userJoinStatus: " + userJoinStatus);
-                                if(userJoinStatus){
-                                    //Update Locally
-                                    UserdetailsUtil.inactivity = true;
-                                    UserdetailsUtil.activityID = SortedlistclassUtil.activity_to_be_displayed;
+            JSONObject userObject = getUserObject(UserdetailsUtil.username,SortedlistclassUtil.activity_to_be_displayed);
 
-                                    //For Debugging
-                                    Log.d("UserDBUpdate", "Successfully Updated User to reflect Joined Activity");
-                                    Log.d("UserDBUpdate", " : " + UserdetailsUtil.inactivity);
-                                    Log.d("UserDBUpdate", UserdetailsUtil.activityID);
-
-                                }
-                            } catch (JSONException e) {
-                                Log.d("UserDBUpdate", "UNSuccessfully Updated User to reflect Joined Activity");
-                                e.printStackTrace();
-                            }
-                        }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(Joined.this, "Connection error while joining, try again later!", Toast.LENGTH_SHORT).show();
-                    error.printStackTrace();
-                }
-            });
-            ao.add(user_join);
+            updateUserDB(userObject);
 
         }else {
             Toast.makeText(Joined.this, "Sorry, you are already in an activity!", Toast.LENGTH_SHORT).show();
